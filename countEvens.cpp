7d54a62b124c9d3a3018145bc62b82af3231b16f
Jorge Medina <jim@umail.ucsb.edu>
2020-02-16 20:14:11 -0800

    final

--- countEvens.cpp
+++ countEvens.cpp
@@ -1,7 +1,12 @@
 #include "arrayFuncs.h"
 #include <cstdlib>
 #include <iostream>
-
+#include "utility.h"
 int countEvens(int a[], int size) {
-  return 42; // STUB!  Replace with correct code.
+  int count = 0;
+  for (int i = 0; i < size; i++){
+    if( isEven(a[i]) == 1)
+	    count += 1;
+  }
+  return count;
 }
--- maxOfArray.cpp
+++ maxOfArray.cpp
@@ -3,5 +3,14 @@
 #include <iostream>
 
 int maxOfArray(int a[], int size) {
-  return -42; // STUB!  Replace with correct code.
+  if(size <1)
+	  exit(1);
+  int max = 0; // STUB!  Replace with correct code.
+  for (int i = 0; i < size; i++){
+	  if(a[i] > max)
+		  max = a[i];
+  
+  }
+  return max;
 }
+
