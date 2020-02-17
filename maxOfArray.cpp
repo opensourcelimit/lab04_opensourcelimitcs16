1f35cb204df24c86f0a0661a85e72a5414182d80
Jorge Medina <jim@umail.ucsb.edu>
2020-02-16 20:27:01 -0800

    final2

--- maxOfArray.cpp
+++ maxOfArray.cpp
@@ -1,10 +1,13 @@
 #include "arrayFuncs.h"
 #include <cstdlib>
 #include <iostream>
+using namespace std;
 
 int maxOfArray(int a[], int size) {
-  if(size <1)
+  if(size <1){
+	  cerr <<"Error: maxOfArray called with size < 1" << endl;
 	  exit(1);
+  }
   int max = 0; // STUB!  Replace with correct code.
   for (int i = 0; i < size; i++){
 	  if(a[i] > max)
